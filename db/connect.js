9ae831bbe43e2c9c33ce7616741284492b6cd015
Yaroslav Saukh <yaroslav2002vas@gmail.com>
2024-12-17 22:59:01 +0200

    mongo connect upd

--- db/connect.js
+++ db/connect.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI, {
-            dbName: process.env.DB_NAME,
-        })
+        const connection = await mongoose.connect(process.env.MONGO_URI)
 
         console.log('Database was successfully connected.')
         return connection
